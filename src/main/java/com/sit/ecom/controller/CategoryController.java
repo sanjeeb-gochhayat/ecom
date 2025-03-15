5022687e400d68e44289d1c65fa9746b6cd613f9
sanjeeb-gochhayat <sanjeebgochhayat1997@gmail.com>
2025-03-15 18:58:29 +0530

    Response Entity added

--- src/main/java/com/sit/ecom/controller/CategoryController.java
+++ src/main/java/com/sit/ecom/controller/CategoryController.java
@@ -5,7 +5,10 @@ import com.sit.ecom.service.CategoryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,8 +34,14 @@ public class CategoryController {
     }
 
     @DeleteMapping("/api/admin/categories/{categoryId}")
-    public String deleteCategory(@PathVariable Long categoryId){
-        return categoryService.deleteCategory(categoryId);
+    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId){
+        try {
+            String status = categoryService.deleteCategory(categoryId);
+            return new ResponseEntity<>(status, HttpStatus.OK);
+        } catch (ResponseStatusException e){
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        }
+
     }
 
 }
--- src/main/java/com/sit/ecom/service/CategoryServiceImpl.java
+++ src/main/java/com/sit/ecom/service/CategoryServiceImpl.java
@@ -1,7 +1,9 @@
 package com.sit.ecom.service;
 
 import com.sit.ecom.entities.CategoryEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,10 +30,7 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public String deleteCategory(Long categoryId) {
-        CategoryEntity category = categories.stream().filter(c -> c.getCategoryId().equals(categoryId)).findFirst().orElse(null);
-        if(category == null){
-            return "category id not found";
-        }
+        CategoryEntity category = categories.stream().filter(c -> c.getCategoryId().equals(categoryId)).findFirst().orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
         categories.remove(category);
         return "Deleted successfully";
     }
